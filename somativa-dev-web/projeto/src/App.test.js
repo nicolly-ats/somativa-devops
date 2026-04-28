332459f263b0ddbaa01ef4e842e27e0f2c1f0a04
Nicolly Teixeira <nicollyats1802@gmail.com>
2026-04-27 22:38:09 -0300

    Add testes

--- somativa-dev-web/projeto/src/App.test.js
+++ somativa-dev-web/projeto/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderiza o título Login', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titulo = screen.getByText(/login/i);
+  expect(titulo).toBeInTheDocument();
+});
+
+test('botão acessar aparece na tela', () => {
+  render(<App />);
+  const botao = screen.getByText(/acessar/i);
+  expect(botao).toBeInTheDocument();
 });
